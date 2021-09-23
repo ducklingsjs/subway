1e3507576b72ff75f5eee071463c60749e5d98d3
Zlatko Štulić <zlatko.stulic@infinum.com>
2021-09-23 13:41:37 +0200

    Add message to slider

--- message-slider.js
+++ message-slider.js
@@ -1,12 +1,22 @@
-const animationInterval = 15000;
-
 setInterval(() => {
-    addMessage('New message added dynamically');
-}, animationInterval);
+    const el = document.getElementsByTagName('li')[0];
+    console.log(document.body.clientWidth);
+    if (el) {
+        console.log(el.getBoundingClientRect());
+    }
+}, 1000)
 
 function addMessage(message) {
     const list = document.getElementById('message-list');
     const listItem = document.createElement('li');
     listItem.appendChild(document.createTextNode(message));
     list.appendChild(listItem);
+}
+
+function deleteMessage(message, elementLeftBound) {
+    const firstMessage = document.getElementsByTagName('li')[0];
+
+    if (firstMessage && firstMessage.getBoundingClientRect().left < 0) {
+
+    }
 }
