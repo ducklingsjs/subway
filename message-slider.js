73da4b8e998a0f7d16181de590d95b0aa2529c8a
Zlatko Štulić <zlatko.stulic@infinum.com>
2021-09-23 13:48:10 +0200

    Remove delete function

--- message-slider.js
+++ message-slider.js
@@ -1,22 +1,6 @@
-setInterval(() => {
-    const el = document.getElementsByTagName('li')[0];
-    console.log(document.body.clientWidth);
-    if (el) {
-        console.log(el.getBoundingClientRect());
-    }
-}, 1000)
-
 function addMessage(message) {
     const list = document.getElementById('message-list');
     const listItem = document.createElement('li');
     listItem.appendChild(document.createTextNode(message));
     list.appendChild(listItem);
-}
-
-function deleteMessage(message, elementLeftBound) {
-    const firstMessage = document.getElementsByTagName('li')[0];
-
-    if (firstMessage && firstMessage.getBoundingClientRect().left < 0) {
-
-    }
 }
